f335aa36a01d42783364e21ebba1fa4e3af22cbb
Jason Dalton <jcdalton@hotmail.com>
2014-01-31 15:22:46 -0600

    first checkin of the reporter

---
+++ htmlTemplate.js
@@ -0,0 +1,107 @@
+exports.htmlTop = '<head> ' +
+'<link rel="stylesheet" type="text/css" href="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css"> ' +
+'<style type="text/css"> ' +
+'body { ' +
+'background-color: #252525; ' +
+'font-size: 20px; ' +
+'-webkit-font-smoothing:antialiased; ' +
+'} ' +
+'.result-box{ ' +
+'float: left; ' +
+'position: relative; ' +
+'width: 510px; ' +
+'height: 280px; ' +
+'margin: 0 0 30px 40px; ' +
+'border-radius: 5px; ' +
+'background-color:#d6ff99; ' +
+'text-align: center; ' +
+'} ' +
+'.result-box-good{ ' +
+'background-color:#d6ff99; ' +
+'}'+
+'.result-box-fail{ ' +
+'background-color:#ffbfac; ' +
+'}'+
+'.result-content{ ' +
+'position: relative; ' +
+'height: 100%; ' +
+'padding: 0 40px; ' +
+'border-radius: 4px; ' +
+'box-shadow: rgba(0,0,0,.2) 0 -5px 0 inset,rgba(0,0,0,.75) 0 1px 8px; ' +
+'text-shadow: rgba(255,255,255,.5) 0 1px 1px; ' +
+'} ' +
+'.result-content-good{ ' +
+'box-shadow: rgba(0,0,0,.2) 0 -5px 0 inset,rgba(0,0,0,.75) 0 1px 8px,rgba(77,117,21,.5) 0 0 70px inset; ' +
+'} ' +
+'.result-content-fail{ ' +
+'box-shadow: rgba(0,0,0,.2) 0 -5px 0 inset,rgba(0,0,0,.75) 0 1px 8px,rgba(215,0,40,.3) 0 0 70px inset; ' +
+'} ' +
+'.modal-header { ' +
+'background-color: #FEFEFA; ' +
+'} ' +
+'</style> ' +
+'</head> ' +
+' ' +
+'<body ng-app="reportApp"> ' +
+'<div class="container" ng-controller="ReportCtrl"> ' +
+'<h1>{{reportTitle}}</h1>' +
+'<div class="row"> ' +
+'<div class="col-md-2 result-box" ng-class="{\'result-box-fail\': result.error > 0}" ng-repeat="result in results"> ' +
+'<div class="row result-content" ng-class="{\'result-content-fail\': result.error > 0, \'result-content-good\': result.error < 1}" ng-click="showDetail(result.name)"> ' +
+'<div class="col-md-12" style="border-bottom: 1px solid"> ' +
+'<div><h3>Suite:</h3> <b>{{result.name}}</b></div> ' +
+'<div>{{test}}</div> ' +
+'</div> ' +
+'<div class="col-md-12"> ' +
+'<div style="color:green">success <b>{{result.success}}</b></div> ' +
+'</div> ' +
+'<div class="col-md-12"> ' +
+'<div style="color:red">error <b>{{result.error}}</b></div> ' +
+'</div> ' +
+'<div class="col-md-12" > ' +
+'<div style="color:orange">skipped <b>{{result.skipped}}</b></div> ' +
+'</div> ' +
+'</div>  ' +
+'</div> ' +
+'<div class="modal fade" id="detailModal"> ' +
+'<div class="modal-dialog"> ' +
+'<div class="modal-content"> ' +
+'<div class="modal-header"> ' +
+'<span>{{detail.name}}</span> ' +
+'</div> ' +
+'<div class="modal-body"> ' +
+'<ul class="list-unstyled"> ' +
+'<li ng-repeat="item in detail.tests"> ' +
+'<span class="label " ng-class="{\'label-success\': item.success, \'label-danger\': !item.success}"> {{item.description}} </span> ' +
+'</li> ' +
+'</ul> ' +
+'</div> ' +
+'<div class="modal-footer"></div> ' +
+'</div> ' +
+'</div> ' +
+'</div> ' +
+'</div> ' +
+' ' +
+'</div> ' +
+' ' +
+'</div> ' +
+'<script src="http://code.jquery.com/jquery-1.10.1.min.js"></script> ' +
+'<script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script> ' +
+'<script src="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script> ' +
+'<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.10/angular.min.js"></script> ' +
+'<script type="text/javascript"> ' +
+'angular.module("reportApp",[]).controller("ReportCtrl", function ($scope) { ' +
+'$scope.results = ';
+
+
+exports.htmlBottom = '$scope.detail = []; '+
+'$scope.showDetail = function (id){ '+
+'var one = document.body.querySelector(\'div#detailModal\'); '+
+'angular.element(\'div#detailModal\').modal(\'show\'); '+
+'$scope.detail = $scope.results[id]; '+
+'} '+
+'}); '+
+'</script> '+
+'</body> ';
+
+
---
+++ index.js
@@ -0,0 +1,129 @@
+'use strict';
+var chalk = require('chalk');
+var os = require('os');
+var path = require('path');
+var fs = require('fs');
+var template = require('./htmlTemplate.js');
+/**
+ * The hyHtmlReporter.
+ *
+ * @param {!object} baseReporterDecorator The karma base reporter.
+ * @param {!object} config The karma config.
+ * @constructor
+ */
+var hyHtmlReporter = function (baseReporterDecorator, config, logger, helper, formatError) {
+    // extend the base reporter
+    baseReporterDecorator(this);
+
+    var self = this;
+    var firstRun = true;
+    var reporterConfig = config.htmlAngularReport || {};
+    var folder = config.reportFolder || 'htmlReport';
+    var baseFolder = config.basePath + '/' + folder;
+    var outputFile = helper.normalizeWinPath(path.resolve(baseFolder, reporterConfig.outputFile|| 'htmlReport.html'));
+    var reportTitle = config.reportTitle || 'Javascript Unit Tests';
+    
+    function formatTimeInterval (time) {
+        var mins = Math.floor(time / 60000);
+        var secs = (time - mins * 60000) / 1000;
+        var str = secs + (secs === 1 ? ' sec' : ' secs');
+
+        if (mins) {
+            str = mins + (mins === 1 ? ' min ' : ' mins ') + str;
+        }
+
+        return str;
+    }
+
+    function repeatString (text, n) {
+        var res = [];
+        var i;
+
+        for (i = 0; i < n; i++) {
+            res.push(text);
+        }
+
+        return res.join('');
+    }
+
+
+    /**
+     * Called each time a test is completed in a given browser.
+     *
+     * @param {!object} browser The current browser.
+     * @param {!object} result The result of the test.
+     */
+    function specComplete (browser, result) {
+        var suiteItem = self.suite[result.suite[0]];
+        if(!suiteItem){
+          suiteItem =self.suite[result.suite[0]] = {};
+          suiteItem.name = result.suite[0];
+          suiteItem.tests = [];
+          suiteItem.success = 0;
+          suiteItem.error = 0;
+          suiteItem.skipped = 0;
+        }
+        var test = {
+            id: result.id,
+            description: result.description,
+            success: result.success,
+            skipped: result.skipped,
+            time: result.time,
+            log: result.log
+        };
+        if(test.success){
+            suiteItem.success++;
+        }
+        if(!test.success){
+            suiteItem.error++;
+        }
+        if(test.skipped){
+            suiteItem.skipped++;
+        }
+        suiteItem.tests.push(test);
+
+      }
+
+    self.specSuccess = specComplete;
+    self.specSkipped = specComplete;
+    self.specFailure = specComplete;
+
+    self.onSpecComplete = function (browser, result) {
+        specComplete(browser, result);
+    };
+
+    self.onRunStart = function (browsers) {
+        self.suite = {};
+        self.allResults = {};
+        self.totalTime = 0;
+        self.numberOfSlowTests = 0;
+        self.numberOfSkippedTests = 0;
+        self.numberOfBrowsers = (browsers || []).length;
+    };
+
+    self.onRunComplete = function (browsers, results) {
+        var page = template.htmlTop;
+        page = page + JSON.stringify(self.suite) + ' ; ';
+        page = page + '$scope.reportTitle = \'' + reportTitle + '\' ; ';
+        page = page + template.htmlBottom;
+        helper.mkdirIfNotExists(path.dirname(outputFile), function (){
+            fs.writeFile(outputFile, page, function (error){
+                if(error){
+                    self.write(chalk.red.underline.bold('Can not write the report:') + '\n');
+                    
+                }else{
+                    self.write(chalk.green.underline.bold('result written to file:' + outputFile) + '\n');
+                }
+            });
+        });
+        
+    };
+};
+
+// inject karma runner baseReporter and config
+hyHtmlReporter.$inject = ['baseReporterDecorator', 'config', 'logger', 'helper', 'formatError'];
+
+// PUBLISH DI MODULE
+module.exports = {
+    'reporter:hy-html': ['type', hyHtmlReporter]
+};
